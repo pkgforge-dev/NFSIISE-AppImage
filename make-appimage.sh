832e240155c02fd428a8fe571b264149f857437c
Link4Electronics <link4electronics@gmail.com>
2026-02-24 19:28:39 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,7 +11,8 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/icons/hicolor/32x32/apps/nfs2se.png
 export DESKTOP=/usr/share/applications/nfs2se.desktop
 export STARTUPWMCLASS=ld-linux.so.2
-export OPENGL=1
+export DEPLOY_OPENGL=1
+export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
 quick-sharun /opt/nfs2se
