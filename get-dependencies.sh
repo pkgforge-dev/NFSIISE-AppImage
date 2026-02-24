4ca8b176908e099f742657cb8f38731221351900
Link4Electronics <link4electronics@gmail.com>
2026-02-24 18:02:04 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,22 +6,17 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-#    lib32-libgl      \
-#    lib32-libx11     \
-#    lib32-libxcursor \
-#    lib32-libxext    \
-#    lib32-libxrender \
+#    lib32-glibc    \
+#    lib32-sdl2     \
 tee -a /etc/pacman.conf <<EOF
 
 [multilib]
 Include = /etc/pacman.d/mirrorlist
 EOF
 pacman -Syu --noconfirm \
-    lib32-glibc      \
     lib32-libpulse \
-    lib32-sdl2 \
-    libdecor         \
-    sdl2             \
+    libdecor       \
+    sdl2           \
     yasm
 
 echo "Installing debloated packages..."
@@ -29,7 +24,6 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package 
 make-aur-package nfs2se-git
 
 # If the application needs to be manually built that has to be done down here
