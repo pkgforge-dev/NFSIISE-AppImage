55b7553794b8ccf3885cc3ae4ac18f136ea9a1b2
Link4Electronics <link4electronics@gmail.com>
2026-08-05 06:11:01 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,13 +9,14 @@ export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export STARTUPWMCLASS=nfs2se
 export DEPLOY_OPENGL=1
-export DEPLOY_PIPEWIRE=1
+#export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/nfs2se
 
 # Additional changes can be done in between here
-#echo 'ANYLINUX_DO_NOT_LOAD_LIBS=libpipewire-0.3.so*:${ANYLINUX_DO_NOT_LOAD_LIBS}' >> ./AppDir/.env
+echo 'ANYLINUX_DO_NOT_LOAD_LIBS=libpipewire-0.3.so*:${ANYLINUX_DO_NOT_LOAD_LIBS}' >> ./AppDir/.env
+echo 'SDL_AUDIODRIVER=alsa' >> ./AppDir/.env
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
