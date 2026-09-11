8cb71ab5d0368dbced7f44531b234b0b362b1e72
Link4Electronics <link4electronics@gmail.com>
2026-09-11 01:44:56 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -22,5 +22,5 @@ echo "$VERSION" > ~/version
 mkdir -p ./AppDir/bin
 cmake -S ./NFSIISE -B build -DCMAKE_BUILD_TYPE=Release ..
 cmake --build build -j$(nproc)
-mv -v "./NFSIISE/Need For Speed II SE/text.*" build/nfs2s "./NFSIISE/Need For Speed II SE/install.win" ./AppDir/bin
+mv -v "./NFSIISE/Need For Speed II SE/"text.* nfs2s "./NFSIISE/Need For Speed II SE/install.win" ./AppDir/bin
 mv -v "./NFSIISE/Need For Speed II SE/nfs2se.conf.template" ./AppDir/bin/nfs2se.conf
